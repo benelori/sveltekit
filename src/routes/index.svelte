23b5da0ab5621c2cd8493b99a0e13c202cc63a90
Lorant BENE <lbene@pitechnologies.ro>
2021-05-03 12:05:17 +0300

    SvelteKit tailwind in dev mode.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,9 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<div class="container flex flex-col">
+    <div class="order-last">yya1</div>
+    <div>yya2</div>
+    <div>yya3</div>
+    <div>yya4</div>
+</div>
---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  mode: 'jit',
+  purge: {
+    content: ['./src/**/*.{html,js,svelte,ts}'],
+  },
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
